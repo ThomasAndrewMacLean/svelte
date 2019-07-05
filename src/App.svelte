ba38c7d92a5b46afd24884cabd2fcb6d372fd931
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2019-07-05 13:31:21 +0200

    try deploing with travis

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,16 @@
 <script>
   export let name;
   let voornaam = "Thomas";
+  let watte = "";
   // import Todos from './Todos.svelte'
 
   const addd = () => {
     console.log(fullName);
+    watte = "😘";
   };
   $: fullName = `${voornaam} ${name}`;
 </script>
 
 <button on:click={addd}>clickkee</button>
-<span>{fullName}</span>
+<h1>{fullName}!!</h1>
+ {watte}
