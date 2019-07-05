1ec809eb86516d3cfe12770b6940a80870c02200
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2019-07-05 13:08:10 +0200

    initial setup 🍌

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let name;
+  let voornaam = "Thomas";
+  // import Todos from './Todos.svelte'
+
+  const addd = () => {
+    console.log(fullName);
+  };
+  $: fullName = `${voornaam} ${name}`;
+</script>
+
+<button on:click={addd}>clickkee</button>
+<span>{fullName}</span>
