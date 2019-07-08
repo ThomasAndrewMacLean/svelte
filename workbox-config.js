2302bd65403ea7920a8f2fcf1bc5ba90bf72685d
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2019-07-08 19:37:58 +0200

    html

--- workbox-config.js
+++ workbox-config.js
@@ -1,6 +1,6 @@
 module.exports = {
     globDirectory: 'public/',
-    globPatterns: ['**/*.{css,js,png,ico,jpeg,webmanifest}'],
+    globPatterns: ['**/*.{css,js,png,ico,jpeg,webmanifest,html}'],
     swDest: 'public/worker.js',
     cleanupOutdatedCaches: true,
 };
