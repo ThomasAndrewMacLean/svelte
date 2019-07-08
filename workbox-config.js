1e2dcc19ac062dabdb151c172438f762d3e5e1e9
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2019-07-08 19:35:09 +0200

    nope

--- workbox-config.js
+++ workbox-config.js
@@ -1,6 +1,6 @@
 module.exports = {
     globDirectory: 'public/',
-    globPatterns: ['**/*.{css,js,png,ico,jpeg,webmanifest,html}'],
+    globPatterns: ['**/*.{css,js,png,ico,jpeg,webmanifest}'],
     swDest: 'public/worker.js',
     cleanupOutdatedCaches: true,
 };
