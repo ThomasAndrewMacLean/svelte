6504e4c215ddd994f67f6b13969bce3d3bb13d9b
ThomasAndrewMacLean <hello@thomasmaclean.be>
2020-09-30 01:43:18 +0200

    sw

--- workbox-config.js
+++ workbox-config.js
@@ -1,6 +1,6 @@
 module.exports = {
     globDirectory: 'public/',
-    globPatterns: ['**/*.{css,js,png,ico,jpeg,webmanifest,html}'],
+    globPatterns: ['**/*.{css,js,png,ico,webmanifest,html}'],
     swDest: 'public/worker.js',
     cleanupOutdatedCaches: true,
     skipWaiting: true,
